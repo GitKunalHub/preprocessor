f6b2e5333647e1bf7b7bb6e92fd7b41195f376a3
GitKunalHub <kunalgolani01@gmail.com>
2025-04-19 15:34:03 +0530

    refactor

--- backend_preprocessing/src/utils/config.py
+++ backend_preprocessing/src/utils/config.py
@@ -6,26 +6,24 @@ load_dotenv()
 
 class Config:
     # Azure File Storage
-    AZURE_STORAGE_CONNECTION_STRING = "DefaultEndpointsProtocol=https;AccountName=chatsee;AccountKey=voz5uIMJNTaFrqvk38g7TUNrzwcYiVeLNfXa9ltPMUixZh3xEz0Wl8gYgopLMWlU4TgzIIB0S2OJ+ASt8xp9kQ==;EndpointSuffix=core.windows.net"
-    AZURE_FILE_SHARE_NAME = "chatsee-fs"
-    AZURE_FILE_DIRECTORY = ""
-    AZURE_BLOB_CONTAINER_NAME = "chatsee-data"
+    AZURE_STORAGE_CONNECTION_STRING = os.getenv("AZURE_STORAGE_CONNECTION_STRING")
+    AZURE_FILE_SHARE_NAME = os.getenv("AZURE_FILE_SHARE_NAME")
+    AZURE_FILE_DIRECTORY = os.getenv("AZURE_FILE_DIRECTORY")
+    AZURE_BLOB_CONTAINER_NAME = os.getenv("AZURE_BLOB_CONTAINER_NAME")  
     # MongoDB
-    # MONGO_USER = quote_plus(os.getenv("MONGODB_USER"))
-    # MONGO_PASS = quote_plus(os.getenv("MONGODB_PASS"))
-    # MONGO_HOST = os.getenv("MONGODB_HOST")
-    # MONGO_PORT = os.getenv("MONGODB_PORT")
-    # MONGO_AUTH_SOURCE = quote_plus(os.getenv("MONGODB_AUTH_SOURCE"))
-    MONGODB_URI = (
-        f"mongodb+srv://dbQH:kunal2001@clusterqh.pvbet.mongodb.net/"
-    )
-    MONGODB_DB = "tenant_ice_energy"
-    MONGODB_COLLECTION = "scheduler_jobs"
+    MONGO_USER = quote_plus(os.getenv("MONGODB_USER"))
+    MONGO_PASS = quote_plus(os.getenv("MONGODB_PASS"))
+    MONGO_HOST = os.getenv("MONGODB_HOST")
+    MONGO_PORT = os.getenv("MONGODB_PORT")
+    MONGO_AUTH_SOURCE = quote_plus(os.getenv("MONGODB_AUTH_SOURCE"))
+    MONGODB_URI = os.getenv("MONGODB_URL")
+    MONGODB_DB = os.getenv("MONGODB_DB")
+    MONGODB_COLLECTION = os.getenv("MONGODB_COLLECTION")
 
     # RabbitMQ
-    RABBITMQ_HOST = "20.244.24.210"
-    RABBITMQ_USER = "chatsee"
-    RABBITMQ_PASS = "chatsee_pro"
-    RABBITMQ_PORT = "5672"
-    REDIS_HOST = "redis-test"
-    REDIS_PORT = 6379
+    RABBITMQ_HOST = os.getenv("RABBITMQ_HOST")
+    RABBITMQ_USER = os.getenv("RABBITMQ_USER")
+    RABBITMQ_PASS = os.getenv("RABBITMQ_PASS")
+    RABBITMQ_PORT = os.getenv("RABBITMQ_PORT")
+    REDIS_HOST = os.getenv("REDIS_HOST")
+    REDIS_PORT = os.getenv("REDIS_PORT")
